a830e5a5cd252b3e249b6ecdbc2e1f5cc35c72dc
aisuvro <2842856+aisuvro@users.noreply.github.com>
2025-03-14 06:10:24 +0600

    Add initial project structure with assets, configuration, and routing

---
+++ .history/resources/views/livewire/home.blade_20250314054651.php
@@ -0,0 +1,183 @@
+
+<div id="myCarousel" class="carousel slide carousel-fade mb-6" data-bs-ride="carousel" data-bs-theme="light">
+    <div class="carousel-inner">
+
+        <!-- Carousel Item 1 -->
+        <div class="carousel-item active">
+            <div class="container">
+                <div class="carousel-caption text-start">
+                    <div class="row">
+
+                        <!-- Carousel Image -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="position-relative h-100">
+                                <div class="carousel-obj">
+                                    <img src="src/assets/img/lamp-2.png" alt="ramadan-lamp" class="obj-1 fade fade-down">
+                                    <img src="src/assets/img/lamp-1.png" alt="ramadan-lamp" class="obj-2 fade fade-down fade-1500">
+                                </div>
+                                <div class="carousel-media">
+                                    <img src="https://placehold.co/400x440" alt="" class="carousel-img">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carousel Content -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="carousel-content">
+                                <div class="carousel-desc">
+                                    <div class="carousel-dets">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_left d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Iftar Time</span>
+                                                        <p class="carousel-dets_time fade fade-up fade-1500">6:45 PM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/iftar.png" class="fade fade-down-left" alt="iftar-time">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_right d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Suhoor Time</span>
+                                                        <p class="carousel-dets_time fade fade-up fade-1500">3:40 AM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/suhoor.png" class="fade fade-down-left" alt="suhoor-time">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-desc-ttl fade fade-left">
+                                        Welcome Ramadan Kareem
+                                    </div>
+                                    <p class="carousel-desc-txt fade fade-left fade-1500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Carousel Item 2 -->
+        <div class="carousel-item">
+            <div class="container">
+                <div class="carousel-caption text-start">
+                    <div class="row">
+
+                        <!-- Carousel Image -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="position-relative h-100">
+                                <div class="carousel-obj">
+                                    <img src="src/assets/img/lamp-2.png" alt="ramadan-lamp" class="obj-1 fade fade-down">
+                                    <img src="src/assets/img/lamp-1.png" alt="ramadan-lamp" class="obj-2 fade fade-down fade-1500">
+                                </div>
+                                <div class="carousel-media">
+                                    <img src="https://placehold.co/400x440" alt="" class="carousel-img">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carousel Content -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="carousel-content">
+                                <div class="carousel-desc">
+                                    <div class="carousel-dets">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_left d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Iftar Time</span>
+                                                        <p class="carousel-dets_time fade fade-up fade-1500">6:45 PM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/iftar.png" class="fade fade-down-left" alt="iftar-time">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_right d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Suhoor Time</span>
+                                                        <p class="carousel-dets_time  fade fade-up fade-1500">3:40 AM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/suhoor.png" class="fade fade-down-left" alt="suhoor-time">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-desc-ttl fade fade-left">
+                                        Welcome Ramadan Kareem
+                                    </div>
+                                    <p class="carousel-desc-txt fade fade-left fade-1500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Carousel Item 3 -->
+        <div class="carousel-item">
+            <div class="container">
+                <div class="carousel-caption text-start">
+                    <div class="row">
+
+                        <!-- Carousel Image -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="position-relative h-100">
+                                <div class="carousel-obj">
+                                    <img src="src/assets/img/lamp-2.png" alt="ramadan-lamp" class="obj-1 fade fade-down">
+                                    <img src="src/assets/img/lamp-1.png" alt="ramadan-lamp" class="obj-2 fade fade-down fade-1500">
+                                </div>
+                                <div class="carousel-media">
+                                    <img src="https://placehold.co/400x440" alt="" class="carousel-img">
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Carousel Content -->
+                        <div class="col-xs-12 col-md-6">
+                            <div class="carousel-content">
+                                <div class="carousel-desc">
+                                    <div class="carousel-dets">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_left d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Iftar Time</span>
+                                                        <p class="carousel-dets_time fade fade-up fade-1500">6:45 PM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/iftar.png" class="fade fade-down-left" alt="iftar-time">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="carousel-dets_right d-flex align-items-center justify-content-between">
+                                                    <div>
+                                                        <span class="carousel-dets_ttl fade fade-up">Suhoor Time</span>
+                                                        <p class="carousel-dets_time fade fade-up fade-1500">3:40 AM</p>
+                                                    </div>
+                                                    <img src="src/assets/img/suhoor.png" class="fade fade-down-left" alt="suhoor-time">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-desc-ttl fade fade-left">
+                                        Welcome Ramadan Kareem
+                                    </div>
+                                    <p class="carousel-desc-txt fade fade-left fade-1500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Carousel Indicators -->
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+</div>
